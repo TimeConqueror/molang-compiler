fcaae3a57cbc090b518ccb3a04013de7cff75fe2
TimeConqueror <timeconqueror999@gmail.com>
2023-09-05 22:49:51 +0300

    3.0.4-customized.5: add check while registering domains

--- src/main/java/ru/timeconqueror/molang/custom/QueryDomain.java
+++ src/main/java/ru/timeconqueror/molang/custom/QueryDomain.java
@@ -4,12 +4,15 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class QueryDomain {
     public static final Map<String, String> DOMAINS = new HashMap<>();
 
     public static void register(String property, String domain) {
-        DOMAINS.put(property, domain);
+        if (DOMAINS.put(property, domain) != null) {
+            throw new IllegalArgumentException("Property is already registered: %s".formatted(property));
+        }
     }
 
     @Nullable
